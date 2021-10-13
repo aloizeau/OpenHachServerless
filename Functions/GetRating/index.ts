f0623a5c25b3454b4fe6bfe821fc64dd48d6d6dc
Antoine SEIN <antoine.sein@amadeus.com>
2021-10-13 11:27:42 +0200

    Manage 404 error + TS functions

--- Functions/GetRating/index.ts
+++ Functions/GetRating/index.ts
@@ -3,6 +3,15 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('GetRating function processed a request.');
 
+    let ratings = context.bindings.inputDocument;
+
+    if (ratings.length === 0) {
+        context.res = {
+            status: 404,
+            body: "No ratings"
+        };
+        return;
+    }
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: context.bindings.inputDocument,
--- Functions/GetRatings/index.ts
+++ Functions/GetRatings/index.ts
@@ -1,11 +1,21 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('GetRatings function processed a request.');
-    
+    context.log('GetRating function processed a request.');
+
+    let rating = context.bindings.inputDocument;
+
+    if (rating.length === 0) {
+        context.res = {
+            status: 404,
+            body: "No rating"
+        };
+        return;
+    }
+
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: context.bindings.inputDocument,
+        status: 200,
+        body: rating[0],
         headers: {
             'Content-Type': 'application/json'
         }
