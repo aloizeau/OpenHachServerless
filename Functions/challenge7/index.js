6ee17907a0dea5a2c83ceb296b7ae1a44b6669d7
Jean-Benoit Paux <9682558+jbpaux@users.noreply.github.com>
2021-10-14 16:25:29 +0200

    Update index.js

--- Functions/challenge7/index.js
+++ Functions/challenge7/index.js
@@ -1,22 +1,13 @@
-const { ServiceBusClient } = require("@azure/service-bus");
-
-const connectionString = "Endpoint=sb://challenge8-team3.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=w86Q6Q1BdRjCoxYIeMib2pXvqkcIft4l0U7mGTi772g="
-const topicName = "receipts";
-
 module.exports = async function (context, eventHubMessages) {
     context.log(`JavaScript eventhub trigger function called for message array ${eventHubMessages}`);
     
-    const sbClient = new ServiceBusClient(connectionString);
-    const sender = sbClient.createSender(topicName);
-
     context.bindings.outputDocument = [];
     context.bindings.outputSbTopic = [];
-
+    
     eventHubMessages.forEach((message, index) => {
         context.log(`Processed message ${JSON.stringify(message)}`);
         
         context.bindings.outputDocument.push({...message, id: message.header.salesNumber});
-        try {
         if(message.header.receiptUrl !== null) {
             const pub = {
                 "totalItems": message.details.length,
@@ -27,17 +18,12 @@ module.exports = async function (context, eventHubMessages) {
                 "receiptUrl": message.header.receiptUrl
             }
             const msg = {
-                body: pub,
+                body: JSON.stringify(pub),
                 applicationProperties: {
                     totalCost: pub.totalCost
                 }
             }
-            await sender.sendMessages(msg);
-        }
-        }
-        finally {
-            await sender.close();
-            await sbClient.close();
+            context.bindings.outputSbTopic.push(msg);
         }
     });
 
